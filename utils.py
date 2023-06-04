f81d7308393bc7ad9b5ba755b6fc6ca34da523a4
dude <zlukcss@gmail.com>
2023-06-04 13:52:07 -0300

    add choice-msg and doc

--- utils.py
+++ utils.py
@@ -1,18 +1,30 @@
-def make_question(q: str, border: str):
+def make_question(q: str, border: str) -> str:
+    """
+    Formation of a request.
+    :param q: question
+    :param border: visual design
+    :return: user answer
+    """
 
-    result = input(
+    result: str = input(
         f"{border}\n{q}?\n{border}\n"
     )
     return result
 
 
 def string_to_bool(choice: str) -> bool:
-    translate = {
+    """
+    Translate from string to bool.
+    :param choice: string value
+    :return: bool value or raise ValueError
+    """
+
+    translate: dict = {
         '1': True,
         '2': False,
     }
 
-    result = translate.get(choice)
-    if result is None:
+    if (result := translate.get(choice)) is None:
         raise ValueError("нужно ввести или 1 - если ДА или 2 - НЕТ")
+
     return result
